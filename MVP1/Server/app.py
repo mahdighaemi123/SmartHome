1cf42841aa1d95d225df819380318f39c1cc45c6
“Mahdi Ghaemi” <mahdighaemi123@gmail.com>
2024-01-29 18:15:49 +0330

    MVP1 -> update server run port to 5001

--- MVP1/Server/app.py
+++ MVP1/Server/app.py
@@ -41,4 +41,4 @@ def create_new_data():
     return jsonify({"status": True, "message": "data saved :)"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5001)
