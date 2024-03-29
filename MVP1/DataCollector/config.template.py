c7627722ffd91164642841a572fd99acca80ea7d
“Mahdi Ghaemi” <mahdighaemi123@gmail.com>
2024-03-29 21:26:05 +0330

    MVP1 -> add config template

---
+++ MVP1/DataCollector/config.template.py
@@ -0,0 +1,16 @@
+config = {
+    "name":"DARA_COLLECTOR_1",
+    "server_url":"http://<YourServerIp>:5000/new_data",
+    "wifi_ssid":"<YourWifiSSID>",
+    "wifi_password":"<YourWifiPassword>",
+    "interval":60,
+    "raining_sensor_pin":13,
+    "display_scl_pin":22,
+    "display_sda_pin":21,
+    "display_width":128,
+    "display_height":32,
+    "display_line_height":12,
+    "temphumidity_sensor_sda":4,
+    "temphumidity_sensor_scl":5,
+    "temphumidity_sensor_address":0x44,
+}
