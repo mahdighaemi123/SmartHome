6bd0aa7c6cc79113e62c185c0d5317a557ac829b
“Mahdi Ghaemi” <mahdighaemi123@gmail.com>
2024-04-07 17:52:12 +0330

    MVP 2 -> fix monitor bug for light intensity > 100 with error float + int

--- MVP2/MONITOR/app.py
+++ MVP2/MONITOR/app.py
@@ -21,21 +21,27 @@ displayServer = DisplayServer(
 )
 
 while True:
-    status = monitor.get_status()
-    pprint.pprint(status)
-    print()
 
-    lines = list()
-    lines.append(
-        f'Time:{status["date_time"]["hour"]}:{status["date_time"]["minute"]}:{status["date_time"]["second"]} {status["date_time"]["jweek_day_en_short"]}')
+    try:
+        status = monitor.get_status()
+        pprint.pprint(status)
+        print()
 
-    lines.append(
-        f'Date:{status["date_time"]["jyear"]}/{status["date_time"]["jmonth"]}/{status["date_time"]["jday"]} {status["date_time"]["jmonth_en"]}')
+        lines = list()
+        lines.append(
+            f'Time:{status["date_time"]["hour"]}:{status["date_time"]["minute"]}:{status["date_time"]["second"]} {status["date_time"]["jweek_day_en_short"]}')
 
-    lines.append(
-        f'Temp:{round(status["temp"])}C Humi:{round(status["humidity"])}%')
+        lines.append(
+            f'Date:{status["date_time"]["jyear"]}/{status["date_time"]["jmonth"]}/{status["date_time"]["jday"]} {status["date_time"]["jmonth_en"]}')
 
-    lines.append(f'Rain:{"Now" if status["rain_status"] else ""}{"," if status["tomorrow_rain_status_prediction"] and status["rain_status"] else ""}{"24h" if status["tomorrow_rain_status_prediction"] else ""}{"No-Rain" if not status["tomorrow_rain_status_prediction"] and not status["rain_status"] else ""} Sun:{status["light_intensity"]//1000+"K" if status["light_intensity"] >= 1000 else round(status["light_intensity"])}')
+        lines.append(
+            f'Temp:{round(status["temp"])}C Humi:{round(status["humidity"])}%')
 
-    displayServer.display(lines=lines)
+        lines.append(f'Rain:{"Now" if status["rain_status"] else ""}{"," if status["tomorrow_rain_status_prediction"] and status["rain_status"] else ""}{"24h" if status["tomorrow_rain_status_prediction"] else ""}{"No-Rain" if not status["tomorrow_rain_status_prediction"] and not status["rain_status"] else ""} Sun:{str(status["light_intensity"]//1000)+"K" if status["light_intensity"] >= 1000 else round(status["light_intensity"])}')
+
+        displayServer.display(lines=lines)
+
+    except Exception as e:
+        print("ERROR", e)
+
     time.sleep(0.720)
